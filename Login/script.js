20e8f6b38efbfbfc63d91a482a0717c8a9793936
Ankur <195884167+74nkur@users.noreply.github.com>
2025-04-29 14:06:19 +0530

    Update script.js

--- Login/script.js
+++ Login/script.js
@@ -4,7 +4,6 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
   const email = document.querySelector('input[type="email"]').value;
   const password = document.querySelector('input[type="password"]').value;
   const rememberMe = document.getElementById('rememberMe').checked;
-
   console.log('Email:', email);
   console.log('Password:', password);
   console.log('Remember Me:', rememberMe);
